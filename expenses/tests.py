c422e2a2d14180a25120a37df75787f78ccb7c44
HusanboyUs <husanghost@gmail.com>
2022-10-21 21:15:39 +0200

    added some tests

--- expenses/tests.py
+++ expenses/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase,Client
+from django.urls import reverse
+from expenses.models import Category,Expense
+import datetime
+
+class TestView(TestCase):
+
+    def setUp(self):
+        self.client=Client()
+        self.expense_list_url=reverse('expense/list/')
+        self.category_list_url=reverse('category/list/')
+
+        Category.objects.create(
+            name='Travelling'
+        )
+    
+
+    def category_list_GET(self):
+        response=self.client.get(self.category_list_url)
+        self.assertEqual(response.status_code, 200)
+
+    def expense_list_GET(self):
+        response=self.client.get(self.expense_list_url)
+        self.assertEqual(response.status_code, 200) 
+
+    def expense_create(self):
+        response=self.client.get(self.expense_list_url)
+        self.assertEquals(response.status_code, 302)
+        self.assertGreater(Category.objects.all().count(),4)
